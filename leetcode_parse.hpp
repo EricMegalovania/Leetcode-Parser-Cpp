389022ce16d8ab19b3deb90c77448f250ee5401e
ericmegalovania <1486853661@qq.com>
2025-12-18 12:54:26 +0800

    finish coding and testing

---
+++ leetcode_parse.hpp
@@ -0,0 +1,212 @@
+#ifndef __LEETCODE_PARSER_HPP__
+#define __LEETCODE_PARSER_HPP__
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <stdexcept>
+
+namespace leetcode_parser {
+
+	namespace internal_basic {
+		inline std::string readLine(std::istream& is) {
+			std::string line;
+			while(std::getline(is, line)){
+				if (std::any_of(line.begin(), line.end(), [](char c)-> bool { 
+					return !std::isspace(c); 
+				})) {
+					return line;
+				}
+			}
+			return "";
+		}
+		inline void skipWhitespace(std::istream& is) {
+			while (std::isspace(is.peek())) is.get();
+		}
+		inline char peekNextChar(std::istream& is) {
+			return static_cast<char>(is.peek());
+		}
+		inline bool isVectorStart(char ch) {
+			return ch == '[';
+		}
+		inline bool isVectorEnd(char ch) {
+			return ch == ']';
+		}
+		inline bool isSeparator(char ch) {
+			return ch == ',';
+		}
+	} // namespace internal_basic
+	
+	namespace internal_parser{
+		using namespace internal_basic;
+
+		template <typename T>
+		T _parse(std::istream& is) {
+			skipWhitespace(is);
+			T value; is >> value;
+			return value;
+		}
+		
+		template <>
+		inline bool _parse<bool>(std::istream& is) {
+			skipWhitespace(is);
+			std::string str;
+			is >> str;
+			if (str == "true") {
+				return true;
+			} else if (str == "false") {
+				return false;
+			} else {
+				throw std::invalid_argument("Invalid boolean value: " + str);
+			}
+		}
+		
+		template <>
+		inline std::string _parse<std::string>(std::istream& is) {
+			std::string result; char ch;
+			skipWhitespace(is);
+			is >> ch;
+			if (ch != '"') {
+				throw std::invalid_argument("String must START with a quote");
+			}
+			bool end_with_quote = false;
+			while (is.get(ch)) {
+				if (ch == '"') {
+					end_with_quote = true;
+					break;
+				}
+				if (ch == '\\') {
+					if (is.get(ch)) {
+						switch (ch) {
+							case 'n': result += '\n'; break;
+							case 't': result += '\t'; break;
+							case 'r': result += '\r'; break;
+							case '\\': result += '\\'; break;
+							case '"': result += '"'; break;
+							default: result += ch; break;
+						}
+					}
+				} else {
+					result += ch;
+				}
+			}
+			if (!end_with_quote) {
+				throw std::invalid_argument("String must END with a quote");
+			}
+			return result;
+		}
+		
+		template <>
+		inline char _parse<char>(std::istream& is) {
+			std::string str = _parse<std::string>(is);
+			if (str.length() != 1) {
+				throw std::invalid_argument("Invalid char input: " + str + ", char length: " + std::to_string(str.length()));
+			}
+			return str[0];
+		}
+		
+		template <typename T>
+		std::vector<T> _parseVector(std::istream& is) {
+			std::vector<T> result;
+			skipWhitespace(is);
+			if (!isVectorStart(peekNextChar(is))) {
+				throw std::invalid_argument("Vector must start with '['");
+			} else {
+				is.get();
+			}
+			
+			skipWhitespace(is);
+			if(is.eof()) {
+				throw std::invalid_argument("Vector must end with ']'");
+			}
+			if (isVectorEnd(peekNextChar(is))) {
+				is.get();
+				return result;
+			}
+
+			while (true) {
+				skipWhitespace(is);
+				if (isVectorStart(peekNextChar(is))) {
+					throw std::invalid_argument("Unexpected nested vector in vector");
+				} else {
+					result.emplace_back(_parse<T>(is));
+				}
+				
+				skipWhitespace(is);
+				if (isVectorEnd(peekNextChar(is))) {
+					is.get(); break;
+				} else if (isSeparator(peekNextChar(is))) {
+					is.get();
+				} else {
+					throw std::invalid_argument("Expected ',' or ']' after vector element");
+				}
+			}
+			
+			return result;
+		}
+		
+		template <typename T>
+		std::vector<std::vector<T>> _parseVector2D(std::istream& is) {
+			std::vector<std::vector<T>> result;
+
+			skipWhitespace(is);
+			if (!isVectorStart(peekNextChar(is))) {
+				throw std::invalid_argument("2D vector must start with '['");
+			} else {
+				is.get();
+			}
+			
+			skipWhitespace(is);
+			if (isVectorEnd(peekNextChar(is))) {
+				is.get();
+				return result;
+			}
+			
+			while (true) {
+				skipWhitespace(is);
+				if (isVectorStart(peekNextChar(is))) {
+					result.emplace_back(_parseVector<T>(is));
+				} else {
+					throw std::invalid_argument("Expected '[' at start of 2D vector row");
+				}
+				
+				skipWhitespace(is);
+				if (isVectorEnd(peekNextChar(is))) {
+					is.get(); break;
+				} else if (isSeparator(peekNextChar(is))) {
+					is.get();
+				} else {
+					throw std::invalid_argument("Expected ',' or ']' after 2D vector row");
+				}
+			}
+			
+			return result;
+		}
+	} // namespace internal_parser
+	
+	template <typename T>
+	T parse() {
+		std::string line=internal_basic::readLine(std::cin);
+		std::istringstream iss(line);
+		return internal_parser::_parse<T>(iss);
+	}
+	
+	template <typename T>
+	std::vector<T> parseVector() {
+		std::string line=internal_basic::readLine(std::cin);
+		std::istringstream iss(line);
+		return internal_parser::_parseVector<T>(iss);
+	}
+	
+	template <typename T>
+	std::vector<std::vector<T>> parseVector2D() {
+		std::string line=internal_basic::readLine(std::cin);
+		std::istringstream iss(line);
+		return internal_parser::_parseVector2D<T>(iss);
+	}
+	
+} // namespace leetcode_parser
+
+#endif // __LEETCODE_PARSER_HPP__
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,81 @@
-#include<bits/stdc++.h>
-#include"../leetcode_parser.hpp"
-using namespace std;
+#include <bits/stdc++.h>
+#include "../leetcode_parse.hpp"
+#include "vector_printer.hpp"
 
-using LL=long long;
-int main(){
+using namespace std;
+using namespace leetcode_parser;
+using namespace VectorPrinter;
+
+using LL = long long;
+
+int main() {
 	freopen("input.txt","r",stdin);
 	
+	int i1 = parse<int>();
+	int i2 = parse<int>();
+	int i3 = parse<int>();
+	int i4 = parse<int>();
+	cout << "i1: " << i1 << endl;
+	cout << "i2: " << i2 << endl;
+	cout << "i3: " << i3 << endl;
+	cout << "i4: " << i4 << endl;
+	
+	LL ll1 = parse<LL>();
+	LL ll2 = parse<LL>();
+	LL ll3 = parse<LL>();
+	LL ll4 = parse<LL>();
+	cout << "ll1: " << ll1 << endl;
+	cout << "ll2: " << ll2 << endl;
+	cout << "ll3: " << ll3 << endl;
+	cout << "ll4: " << ll4 << endl;
+	
+	bool b1 = parse<bool>();
+	bool b2 = parse<bool>();
+	cout << "b1: " << (b1 ? "true" : "false") << endl;
+	cout << "b2: " << (b2 ? "true" : "false") << endl;
+	
+	double d1 = parse<double>();
+	double d2 = parse<double>();
+	double d3 = parse<double>();
+	cout << "d1: " << d1 << endl;
+	cout << "d2: " << d2 << endl;
+	cout << "d3: " << d3 << endl;
+	
+	char c1 = parse<char>();
+	char c2 = parse<char>();
+	char c3 = parse<char>();
+	cout << "c1: '" << c1 << "'" << endl;
+	cout << "c2: '" << c2 << "'" << endl;
+	cout << "c3: '" << c3 << "'" << endl;
+	
+	string s1 = parse<string>();
+	string s2 = parse<string>();
+	string s3 = parse<string>();
+	cout << "s1: \"" << s1 << "\"" << endl;
+	cout << "s2: \"" << s2 << "\"" << endl;
+	cout << "s3: \"" << s3 << "\"" << endl;
+	
+	vector<int> v_empty = parseVector<int>();
+	vector<int> v_int = parseVector<int>();
+	vector<string> v_string = parseVector<string>();
+	vector<double> v_double = parseVector<double>();
+	printVector("v_empty", v_empty);
+	printVector("v_int", v_int);
+	printVector("v_string", v_string);
+	printVector("v_double", v_double);
+	
+	vector<vector<int>> vv_empty = parseVector2D<int>();
+	vector<vector<int>> vv_1row_0elements = parseVector2D<int>();
+	vector<vector<int>> vv_1row_1element = parseVector2D<int>();
+	vector<vector<int>> vv_2x2 = parseVector2D<int>();
+	vector<vector<int>> vv_3rows = parseVector2D<int>();
+	vector<vector<double>> vv_2x1_double = parseVector2D<double>();
+	printVector2D("vv_empty", vv_empty);
+	printVector2D("vv_1row_0elements", vv_1row_0elements);
+	printVector2D("vv_1row_1element", vv_1row_1element);
+	printVector2D("vv_2x2", vv_2x2);
+	printVector2D("vv_3rows", vv_3rows);
+	printVector2D("vv_2x1_double", vv_2x1_double);
+
 	return 0;
 }
